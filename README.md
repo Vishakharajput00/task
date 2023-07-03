6060ddf636e3e3a87460a4cc5d19c82481add443
Vishakharajput00 <rajputvr00feb@gmail.com>
2023-07-03 12:15:31 +0530

    completed task

---
+++ README.md
@@ -0,0 +1,4 @@
+//first start the json file with 'json-server --watch db.json'
+//then after installing npm start the project with npm start command 
+//after drag the image you can see the changes in browser after refreshing the browser data will not be chnaged it will be same as you drag the imgae 
+//onClick on image you can see the full imgae and after click on esc it will be close ...
---
+++ src/component/Home.js
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { CircularProgress } from '@material-ui/core';
+import axios from 'axios';
+
+const Home = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [lastSaveTime, setLastSaveTime] = useState(null);
+
+
+  const handleImageClick = (image) => {
+      setOpen(true);
+    setSelectedImage(image);
+    console.log(selectedImage,'selectwdimage')
+  };
+const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedImage(null);
+  };
+
+  const [loading, setLoading] = useState(true);
+
+
+  const [cards, setCards] = useState();
+
+  const data = async ()=>{
+    setLoading(true);
+
+    try{
+      const res = await axios({
+        method:"GET",
+        url:'http://localhost:3000/bankdata'
+      });
+      if(res){
+        console.log(res,'res----')
+setCards(res.data)
+setLoading(false);
+      }
+
+    }catch(error){
+      console.log(error)
+      setLoading(false);
+    }
+  }
+ 
+ 
+
+  useEffect(() => {
+    setTimeout(() => {
+      data()
+    }, 1000);
+
+  }, [])
+  
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData('text/plain', index);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  
+
+  const handleDrop =async(e, index) => {
+    const cardIndex = e.dataTransfer.getData('text/plain');
+    const updatedCards = [...cards];
+    const cardToMove = updatedCards[cardIndex];
+    updatedCards.splice(cardIndex, 1);
+    updatedCards.splice(index, 0, cardToMove);
+console.log("dsffge--------------",updatedCards)
+    setCards(updatedCards);
+    
+  for(const i in updatedCards){
+    const{id,title,type,position}=updatedCards[i]
+    await axios.delete(`http://localhost:3000/bankdata/${id}`)
+    await axios.post('http://localhost:3000/bankdata',{id,title,type,position})
+    // setCards(updatedCards)
+
+
+  }
+  alert("data changed ")
+
+
+  };
+
+  return (
+   <>
+    {loading ? (
+        <CircularProgress /> 
+      ) : (
+        <div>
+           <Grid container spacing={2}>
+     {cards && (
+      <>
+       {cards.map((card, index) => (
+        <Grid item key={index} xs={12} sm={6} md={4} lg={4}>
+          <div
+            className="card"
+            draggable
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDragOver={handleDragOver}
+            onDrop={(e) => handleDrop(e, index)}
+          >
+             <img src={card.type} alt={card.title} 
+            key={index}
+          
+            style={{width:"400px",height:"400px"}}
+             onClick={() => handleImageClick(card)}/>
+            <p>{card.title}</p>
+           
+          </div>
+         
+        </Grid>
+         
+      ))}
+      </>
+     )}
+    </Grid>
+        </div>
+      )}
+  
+
+     <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>
+           {selectedImage && 
+          <>
+           <h4>
+            {selectedImage.title}
+           </h4>
+          
+          </>
+         }
+        </DialogTitle>
+        <DialogContent>
+          {selectedImage && 
+          <>
+          
+          <img src={selectedImage.type}  style={{width:"400px",height:"400px"}} />
+          </>
+         }
+        </DialogContent>
+      </Dialog>
+   </>
+  );
+};
+
+export default Home;
+
+  
